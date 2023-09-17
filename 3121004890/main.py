d814f64dcbb46ebaefa5045728b980f8939d6f3c
86138 <86138@DESKTOP-VJ7KAE6>
2023-09-17 16:35:42 +0800

    feat: 增加读文件函数，命令行参数

--- 3121004890/main.py
+++ 3121004890/main.py
@@ -1,1 +1,10 @@
+import sys
 
+def read_file(file_path):
+    with open(file_path,'r') as file:
+        return file.read()
+
+if __name__=='__main__':
+    original_file = sys.argv[1]
+    file_to_check=sys.argv[2]
+    output_file=sys.argv[3]
